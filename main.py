16ba3f60144b7cba446c9a799b6f8942098599b3
Kris Dorohina <sun.08.90@mail.ru>
2024-12-26 15:31:03 +0300

    правка

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///my_site.com'
@@ -8,7 +9,7 @@ db = SQLAlchemy(app)
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    tittle = db.Column(db.String(300), nullable=False)
+    title = db.Column(db.String(300), nullable=False)
     text = db.Column(db.Text, nullable=False)
 
 
@@ -20,11 +21,20 @@ def index():
 @app.route('/create', methods=['POST', 'GET'])
 def create():
     if request.method == 'POST':
-        print(request.form['title'])
-        print(request.form['text'])
+        title = request.form['title']
+        text = request.form['text']
+
+        post = Post(title=title, text=text)
+
+        try:
+            db.session.add(post)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'Произошла ошибка'
     else:
         return render_template('create.html')
-    #return render_template('create.html')
+
 
 
 @app.route('/about')
