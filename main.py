f8cd6a83ff80352a76adea8062088bc125106a34
Kris Dorohina <sun.08.90@mail.ru>
2025-04-01 13:09:29 +0300

    first commit

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from datetime import datetime
 import secrets
 
-from werkzeug.security import generate_password_hash
+from sqlalchemy.orm import backref
+from werkzeug.security import generate_password_hash, check_password_hash
 
-# Основные импорты Flask
+
 from flask import Flask, render_template, redirect, url_for, flash, request
 
-# Flask-WTF для работы с формами
+
 from flask_wtf import FlaskForm, CSRFProtect
 from wtforms import StringField, TextAreaField, FileField, SelectMultipleField, SubmitField
 from wtforms.validators import DataRequired, Length
@@ -18,8 +19,8 @@ from flask_uploads import UploadSet, configure_uploads, IMAGES
 # Flask-SQLAlchemy для работы с базой данных
 from flask_sqlalchemy import SQLAlchemy
 
-# Flask-Login для аутентификации (опционально)
-#from flask_login import LoginManager, UserMixin, login_required, current_user
+# Flask-Login для аутентификации
+from flask_login import LoginManager, UserMixin, login_required, current_user, login_user, logout_user
 
 app = Flask(__name__)
 
@@ -31,7 +32,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///my_site.db'
 db = SQLAlchemy(app)
 photos = UploadSet('photos', IMAGES)
 configure_uploads(app, photos)
-#login_manager = LoginManager(app)  # Flask-Login (опционально)
+login_manager = LoginManager(app)  # Flask-Login
+login_manager.login_view = 'login'
 
 csrf = CSRFProtect(app)
 
@@ -62,12 +64,25 @@ class RecipeForm(FlaskForm):
     submit = SubmitField('Добавить рецепт')
 
 
+class Recipe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(300), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    ingredients = db.Column(db.Text, nullable=False)
+    instructions = db.Column(db.Text, nullable=False)
+    dish_types = db.Column(db.String(300))
+    photo = db.Column(db.String(300))  # Путь к файлу
+    created_at = db.Column(db.DateTime, default=datetime.now)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    user = db.relationship('User', backref('recipes', lazy=True))
+
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(300), nullable=False)
     text = db.Column(db.Text, nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.now)  # Дата создания поста
-    # user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)  # Связь с пользователем
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)  # Связь с пользователем
 
     # Отношение "многие к одному" с моделью User
     # user = db.relationship('User', backref=db.backref('posts', lazy=True))
@@ -80,6 +95,12 @@ class User(db.Model):
     password_hash = db.Column(db.String(128), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.now)
 
+    def get_id(self):
+        return str(self.id)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 @app.route('/index')
 @app.route('/')
@@ -88,19 +109,21 @@ def index():
 
 
 @app.route('/add_recipe', methods=['GET', 'POST'])
+@login_required
 def add_recipe():
     form = RecipeForm()
     if form.validate_on_submit():
         filename = photos.save(form.photo.data)
 
         # Создание объекта рецепта
-        recipe = RecipeForm(
+        recipe = Recipe(
             title=form.title.data,
             description=form.description.data,
             ingredients=form.ingredients.data,
             instructions=form.instructions.data,
             dish_types=", ".join(form.dish_types.data),  # Сохраняем выбранные типы блюд
-            photo=filename
+            photo=filename,
+            user_id=current_user.id,
         )
         # Сохранение в базу данных
         try:
@@ -124,12 +147,14 @@ def registration():
         password = request.form['password']
         # Проверяем, что все поля заполнены
         if not username or not email or not password:
-            return 'Все поля должны быть заполнены'
+            flash('Все поля должны быть заполнены', 'danger')
+            return redirect(url_for('registration'))
 
         # Проверяем, что пользователь с таким именем или email уже не существует
         existing_user = User.query.filter((User.username == username) | (User.email == email)).first()
         if existing_user:
-            return 'Пользователь с таким именем или email уже существует'
+            flash('Пользователь с таким именем или email уже существует', 'danger')
+            return redirect(url_for('registration'))
 
         # Хэшируем пароль
         password_hash = generate_password_hash(password)
@@ -139,21 +164,37 @@ def registration():
         try:
             db.session.add(user)
             db.session.commit()
-            return redirect('/login')
+            flash('Регистрация прошла успешно! Теперь вы можете войти.', 'success')
+            return redirect(url_for('/login'))
         except Exception as e:
             db.session.rollback()  # Откатываем транзакцию в случае ошибки
             print(f"Ошибка: {e}")
             return 'произошла ошибка'
-
-
-    else:
-        return render_template('registration.html')
+    return render_template('registration.html')
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        user = User.query.filter_by(username=username).first()
+
+        if user and check_password_hash(user.password_hash, password):
+            login_user(user)
+            flash('Вчод выполнен успешно!', 'succes')
+            return redirect(url_for('index'))
+        else:
+            flash('Неверное имя пользователя или пароль', 'danger')
     return render_template('login.html')
 
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('Вы вышли из системы', 'success')
+    return redirect(url_for('index'))
 
 @app.route('/account')
 def personal_account():
